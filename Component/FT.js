d7f427068a121508517ade735d70cbdaf1b3ad24
dawood zia <dawood@gtechsources.com>
2025-04-08 13:25:39 +0500

    D

---
+++ Component/FT.js
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+const FT = () => {
+  return (
+    <View>
+      <Text>FT</Text>
+    </View>
+  );
+};
+
+export default FT;
